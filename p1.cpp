103dd450ef21e52d3e2e2aa1ef530553f672051e
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:14:01 -0500

    first comit

---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"ingresa primer numero "<<endl;
+cin>>a;
+cout<<"ingresa segundo numero "<<endl;
+cin>>b;
+c=a+b;
+cout<<"la suma de ambos numeros es: "<<c;
+
+return 0;
+}
+
